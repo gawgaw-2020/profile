80c0d18af26d56857ded7a8e4267d6260b4e4967
takuya_jp <togawa61@gmail.com>
2020-04-16 00:53:01 +0900

    フロントページ完成

--- js/swiper.js
+++ js/swiper.js
@@ -2,15 +2,32 @@ var swiper = new Swiper('.swiper-container', {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
-  },
+    },
   loop: true,
+  speed: 500,
+  spaceBetween: 10,
+  centeredSlides: true,
   pagination: {
     el: '.swiper-pagination',
     type: 'bullets',
     clickable: true,
   },
   autoplay: {
-    delay: 3000,
-    disableOnInteraction: true
+    delay: 5000,
   },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+      speed: 1000,
+      autoplay: {
+        delay: 5000,
+      },
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      speed: 1500,
+    }
+  }
 });
